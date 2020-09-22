267631d2d365b5f90e7b1f2f8dfd5450cc0ec5c0
moonee <uuuu118@naver.com>
2020-09-22 09:40:15 +0900

    Add view methods

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,26 +1,31 @@
+
 // Pomodoro 시작
 // Pomodoro 멈춤
 
-
+const audio = new Audio('./resources/alertSound.wav');
 // Pomodoro 정보 저장 담당
-var PomodoroApp = function(PomodoroView){
+var PomodoroApp = function(){
     var startTime;
     var afterTime;
     var distance;
     var min, sec;
-    var startPomo=()=>{ // 처음 시작 
+    var startPomo=(time)=>{ // 처음 시작 
         startTime=new Date();
         afterTime=new Date(startTime);
-        afterTime.setMinutes(startTime.getMinutes()+20);
+        afterTime.setMinutes(startTime.getMinutes()+time);
         changeTime();
     }
     var changeTime=()=>{ // 남은 시간 변경 
+        audio.play();
         var x = setInterval(() => {
+            startTime=new Date();
             distance = afterTime.getTime()-startTime.getTime();
             min = Math.floor((distance%(1000*60*60))/(1000*60));
             sec = Math.floor((distance%(1000*60))/1000);
-            if(distance <0){
+            app.drawTime(min,sec);
+            if(distance <=0){
                 clearInterval(x);
+                app.controlPomo();
             }
         }, (1000));
     }
--- View.js
+++ View.js
@@ -1,8 +1,42 @@
 
 // 렌더링 
 var PomodoroView = function(PomodoroApp){
-    document.querySelector('.startBtn').addEventListener("click",()=>{
-        document.querySelector('.contents').innerHTML=`<div class="leftTime"></div>
-        <span>후에 완료됩니다...</span>`;
+
+    let PomoNum;
+    const beep=()=>{
+        audio.play(); 
+    }
+    document.querySelector('.contents').addEventListener("click",(e)=>{
+        if(e.target.className="startBtn"){
+            PomoNum=1;
+            PomodoroApp().startPomo(2);
+        }
     })
+
+
+    const drawTime =(min,sec)=>{
+        document.querySelector('.contents').innerHTML=`
+        <span class="title">재배중인 토마토</span>
+        <div class="leftTime"></div>
+        <span>${min} : ${sec}후에 완료됩니다...</span>`;
+    }
+
+    const controlPomo=()=>{
+        PomoNum++;
+        if(PomoNum<=2){
+            if(PomoNum%2!==0){ // work 
+                PomodoroApp().startPomo(2);
+                beep();
+            }
+            else{
+                PomodoroApp().startPomo(1);
+                beep();
+            }
+        }
+        else{
+            document.querySelector('.contents').innerHTML=` <button class="startBtn">재배시작하기</button>`
+        }
+    }
+
+    return {drawTime,controlPomo};
 };
